4f3c55b45748ca25ba7b9263521e8db6ccec01d0
Jinhyuk Kim <reniowood@gmail.com>
2020-01-11 00:37:33 -0800

    Add Board, Game structs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+struct Board {
+    width: u32,
+    height: u32,
+    values: Vec<Vec<Option<u32>>>
+}
+
+struct Game {
+    goal: u32,
+    board: Board,
+}
+
 fn main() {
     println!("Hello, world!");
 }

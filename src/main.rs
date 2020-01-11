1617fdab559d8f8c58baade7ee0bce3e131b99d9
Jinhyuk Kim <reniowood@gmail.com>
2020-01-11 01:04:17 -0800

    Add methods to Board and Game struct

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,49 @@
 struct Board {
     width: u32,
     height: u32,
-    values: Vec<Vec<Option<u32>>>
+    blocks: Vec<Vec<Option<u32>>>
+}
+
+impl Board {
+    fn new(width: u32, height: u32) -> Board {
+        Board {
+            width: width, 
+            height: height,
+            blocks: vec![vec![None; width as usize]; height as usize],
+        }
+    }
+
+    fn move_blocks(&self, mv: &Move) {
+    }
 }
 
 struct Game {
     goal: u32,
     board: Board,
+}
+
+impl Game {
+    fn new(goal: u32, width: u32, height: u32) -> Game {
+        Game {
+            goal: goal,
+            board: Board::new(width, height),
+        }
+    }
+
+    fn win(&self) -> bool {
+        false
+    }
+
+    fn lose(&self) -> bool {
+        false
+    }
+
+    fn play(&self, mv: &Move) {
+    }
+}
+
+enum Move {
+    Up, Down, Left, Right,
 }
 
 fn main() {
